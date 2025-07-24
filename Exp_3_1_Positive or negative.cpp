61db147c252d603693ff5a924b80d0452f794741
rg861348 <rg861348@gmail.com>
2025-07-24 12:20:45 +0530

    Create Exp_3_1_Positive or negative.cpp

---
+++ Exp_3_1_Positive or negative.cpp
@@ -0,0 +1,33 @@
+// RISHITA GOSWAMI 
+// 24070123086
+// B1
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter a number:";
+    cin >> a;
+    if(a > 0)
+    {
+        cout << "Entered Number is Positive"; 
+    }
+    else if(a < 0) 
+    {
+        cout << "Entered Number is Negative"; 
+    }
+    else 
+    {
+        cout << "Entered Number is 0"; 
+    }
+    return 0;
+}
+
+/*
+output:
+Enter Number: 2
+Entered Number is Positive
+
+Enter a number:-5
+Entered Number is Negative
+*/
